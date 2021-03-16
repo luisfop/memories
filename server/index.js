ad0167c70d869399deeab96d8f5420ef87544cdf
luisop <luisfop77@gmail.com>
2021-03-16 11:16:06 -0300

    intial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+
