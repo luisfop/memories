b53d2613584d8d394020daad7da491863fa0a8d0
luisop <luisfop77@gmail.com>
2021-03-19 18:02:14 -0300

    started the implementation of redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from './App';
+import App from "./App";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-ReactDom.render(<App />, document.getElementById('root'));
+import reducers from "./reducers/index";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
